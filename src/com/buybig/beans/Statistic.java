c3f5e7a885bbfd0f33000024884b0c3c9c88d35f
Frédéric Saucier-Naveros <Admin@Admin-PC>
2014-11-07 18:25:02 -0500

    Test

--- src/com/buybig/beans/Statistic.java
+++ src/com/buybig/beans/Statistic.java
@@ -2,20 +2,18 @@ package com.buybig.beans;
 
 public class Statistic {
 
-	private  int nbSessions;
+	private int nbSessions;
+
 	/*
-	C. Utilisé un bean pour indiquer (sur votre page principale du site) 
-	combien il y a de session en cours (c'est à dire: combien 
-			d'utilisateur sont en train de magasiner actuellement).
-	*/
+	 * C. Utilisé un bean pour indiquer (sur votre page principale du site)
+	 * combien il y a de session en cours (c'est à dire: combien d'utilisateur
+	 * sont en train de magasiner actuellement).
+	 */
 	public int getNbSessions() {
 		return nbSessions;
 	}
-	
 
 	public void setNbSessions(int nbSessions) {
 		this.nbSessions = nbSessions;
 	}
-	
-	////
 }

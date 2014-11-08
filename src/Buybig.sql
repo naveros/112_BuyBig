8bfae300ce6a4fff2800e41ad99f779ca773edfa
Frédéric Saucier-Naveros <Admin@Admin-PC>
2014-11-07 23:23:10 -0500

    update sql

--- src/Buybig.sql
+++ src/Buybig.sql
@@ -3,42 +3,43 @@ DROP SEQUENCE SEQ_ID_BOOK;
 DROP SEQUENCE SEQ_ID_PURCHASE;
 DROP SEQUENCE SEQ_ID_ORDER;
 
-DROP TABLE user 		CASCADE CONSTRAINTS PURGE;
-DROP TABLE book        	CASCADE CONSTRAINTS PURGE;
-DROP TABLE purchase    	CASCADE CONSTRAINTS PURGE;
-DROP TABLE order    	CASCADE CONSTRAINTS PURGE;
+DROP TABLE USER 		CASCADE CONSTRAINTS PURGE;
+DROP TABLE BOOK        	CASCADE CONSTRAINTS PURGE;
+DROP TABLE PURCHASE    	CASCADE CONSTRAINTS PURGE;
+DROP TABLE ORDER    	CASCADE CONSTRAINTS PURGE;
 
 CREATE SEQUENCE SEQ_ID_USER      	START WITH 1 INCREMENT BY 1;
 CREATE SEQUENCE SEQ_ID_BOOK       	START WITH 1 INCREMENT BY 1;
 CREATE SEQUENCE SEQ_ID_PURCHASE     START WITH 1 INCREMENT BY 1;
 CREATE SEQUENCE SEQ_ID_ORDER 		START WITH 1 INCREMENT BY 1;
 
-CREATE TABLE user (idUser   NUMBER       CHECK (idMembre > 0),
-                     username   VARCHAR(100) NOT NULL,
-                     firstname  VARCHAR(100) NOT NULL,
-                     lastname   VARCHAR(100) NOT NULL,
-                     adress		VARCHAR(100) NOT NULL,      
-                     CONSTRAINT pk_user    PRIMARY KEY (idbook));
+CREATE TABLE USER (IDUSER   NUMBER       CHECK (idMembre > 0),
+                     USERNAME   VARCHAR(100) NOT NULL,
+                     FIRSTNAME  VARCHAR(100) NOT NULL,
+                     LASTNAME   VARCHAR(100) NOT NULL,
+                     ADRESSE		VARCHAR(100) NOT NULL,      
+                     CONSTRAINT PK_USER    PRIMARY KEY (IDBOOK));
 
-CREATE TABLE book (idBook         	NUMBER       CHECK (idBook > 0),
-                    title           VARCHAR(100) NOT NULL,
-                    isbn     		VARCHAR(100) NOT NULL,
-                    author          VARCHAR(100) NOT NULL,
-                    price           NUMBER    NOT NULL,
-                    CONSTRAINT      pk_book     PRIMARY KEY (idBook));
+CREATE TABLE BOOK (IDBOOK         	NUMBER       CHECK (IDBOOK > 0),
+                    TITLE           VARCHAR(100) NOT NULL,
+                    ISBN     		VARCHAR(100) NOT NULL,
+                    AUTHOR          VARCHAR(100) NOT NULL,
+                    PRICE           NUMBER    NOT NULL,
+                    CATEGORY		VARCHAR(100) NOT NULL,
+                    CONSTRAINT      PK_BOOK     PRIMARY KEY (IDBOOK));
 
-CREATE TABLE purchase (idPurchase     NUMBER     CHECK (idPret > 0),
-                   idUser   NUMBER     CHECK (idUser > 0),
-                   idBook    NUMBER     CHECK (idBook > 0),
-                   purchaseDate   TIMESTAMP,
-                   CONSTRAINT pk_perchase PRIMARY KEY (idPret),
-                   CONSTRAINT fk_purchase_user    FOREIGN KEY (idUser) 	REFERENCES user (idUser)   ON DELETE CASCADE,
-                   CONSTRAINT fk_pucchase_book    FOREIGN KEY (idBook)  REFERENCES book (idBook)   ON DELETE CASCADE);
+CREATE TABLE PURCHASE (idPURCHASE     NUMBER     CHECK (IDPRET > 0),
+                   IDUSER   NUMBER     CHECK (IDUSER > 0),
+                   IDBOOK    NUMBER     CHECK (IDBOOK > 0),
+                   PURCHASEDate   TIMESTAMP,
+                   CONSTRAINT PK_perchase PRIMARY KEY (IDPRET),
+                   CONSTRAINT PK_PURCHASE_USER    FOREIGN KEY (IDUSER) 	REFERENCES USER (IDUSER)   ON DELETE CASCADE,
+                   CONSTRAINT FK_pucchase_BOOK    FOREIGN KEY (IDBOOK)  REFERENCES BOOK (IDBOOK)   ON DELETE CASCADE);
 
-CREATE TABLE order (idOrder  NUMBER      CHECK (idReservation > 0),
-                          idUser         NUMBER     CHECK (idUser > 0),
-                          idBook         NUMBER     CHECK (idBook > 0),
+CREATE TABLE ORDER (IDORDER  NUMBER      CHECK (IDORDER > 0),
+                          IDUSER         NUMBER     CHECK (IDUSER > 0),
+                          IDBOOK         NUMBER     CHECK (IDBOOK > 0),
                           dateAdded		 TIMESTAMP,
-                          CONSTRAINT      pk_order  PRIMARY KEY (idOrder),
-                          CONSTRAINT fk_order_user    FOREIGN KEY (idUser) 	REFERENCES user (idUser)   ON DELETE CASCADE,
-                   		  CONSTRAINT fk_order_book    FOREIGN KEY (idBook)  REFERENCES book (idBook)   ON DELETE CASCADE);
+                          CONSTRAINT      PK_ORDER  PRIMARY KEY (IDORDER),
+                          CONSTRAINT FK_ORDER_USER    FOREIGN KEY (IDUSER) 	REFERENCES USER (IDUSER)   ON DELETE CASCADE,
+                   		  CONSTRAINT FK_ORDER_BOOK    FOREIGN KEY (IDBOOK)  REFERENCES BOOK (IDBOOK)   ON DELETE CASCADE);

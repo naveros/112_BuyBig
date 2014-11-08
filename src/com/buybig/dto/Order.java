bf00975be33624658f90a5c80cf65dc75200398b
amzo <amzo@192.168.2.38>
2014-11-07 21:46:11 -0500

    Correction du dto Order.java.

--- src/com/buybig/dto/Order.java
+++ src/com/buybig/dto/Order.java
@@ -1,40 +1,46 @@
 package com.buybig.dto;
 
+import java.sql.Timestamp;
+
 public class Order {
 
 	private String idOrder;
-	private Book book; 
+	private Timestamp dateAdded;
 	private User user;
+	private Book book;
 	
 	public Order(){}
-
-
-	public Book getBook() {
-		return book;
-	}
-
-
-	public void setBook(Book book) {
-		this.book = book;
-	}
-
-
-	public User getUser() {
-		return user;
-	}
-
-
-	public void setUser(User user) {
-		this.user = user;
-	}
-
 
 	public String getIdOrder() {
 		return idOrder;
 	}
 
-
 	public void setIdOrder(String idOrder) {
 		this.idOrder = idOrder;
 	}
+
+	public Timestamp getDateAdded() {
+		return dateAdded;
+	}
+
+	public void setDateAdded(Timestamp dateAdded) {
+		this.dateAdded = dateAdded;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public Book getBook() {
+		return book;
+	}
+
+	public void setBook(Book book) {
+		this.book = book;
+	}
+
 }
